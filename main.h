6c352790c7b95b67e859448cefa1554bcedde423
“Dino” <daringamaleldin@hotmail.com>
2023-09-17 14:57:21 -0700

    update main

--- main.h
+++ main.h
@@ -7,6 +7,20 @@
 #include <limits.h>
 #include <unistd.h>
 
+
+/**
+ * struct format - is the function to check for format
+ * @sp: is the specifier type
+ * @f: is the print function to use
+ */
+
+typedef struct format
+{
+	char *sp;
+	int (*f)();
+}
+format_t;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(va_list args);
