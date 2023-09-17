d0a6050bb6b25f1ccdceb3687511ec295e9c2588
Romisaa <romisaa.hamdy@yahoo.com>
2023-09-17 16:01:23 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,46 @@
 #include "main.h"
 /**
+ * find_op - to find format
+ * @format: is a character string
+ * @args: list of arguments
+ * @ops: array of specifiers
+ * Return: the number of characters printed & -1 otherwise
+ */
+int find_op(const char *format, va_list args, form_t *ops)
+{
+int count = 0;
+int i = 0;
+int j;
+while (format[i] != '\0')
+{
+if (format[i] == '%')
+{
+i++;
+j = 0;
+while (ops[j].sp != NULL)
+{
+if (*(ops[j].sp) == format[i])
+{
+count += ops[j].f(args);
+if (count == -1)
+return (-1);
+break;
+}
+j++;
+}
+if (ops[j].sp == NULL)
+{
+count++;
+count += _putchar(format[i]);
+}
+}
+else
+count += _putchar(format[i]);
+i++;
+}
+return (count);
+}
+/**
  * _printf - print anthing according to format
  * @format: is a character string
  * Return: the number of characters printed & -1 otherwise
