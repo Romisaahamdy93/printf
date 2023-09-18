edb573ebbda0ab0110d3d6f077bef8a493f25c37
Romisaa <romisaa.hamdy@yahoo.com>
2023-09-18 08:05:31 -0700

    edit

--- main.h
+++ main.h
@@ -26,5 +26,14 @@ int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int print_percent(void);
-
+int print_int(va_list args);
+int print_dec(va_list args);
+int print_binary(va_list args);
+int print_unsigned(va_list args);
+int print_hex(va_list args);
+int print_HEX(va_list args);
+int print_S(va_list args);
+int print_ptr(va_list args);
+int print_rev(va_list args);
+int print_rot13(va_list args);
 #endif
