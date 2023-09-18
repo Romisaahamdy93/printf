c2ec41c238f903b336c0f8e8e59532170039ca47
Romisaa <romisaa.hamdy@yahoo.com>
2023-09-18 16:25:15 -0700

    task1

--- task1.c
+++ task1.c
@@ -13,15 +13,16 @@ unsigned int num;
 x = va_arg(args, int);
 if (x < 0)
 {
-count = count + _putchar('-');
-x = -x;
+count += _putchar('-');
+num = x * -1 ;
 }
+else
 num = x;
 while (num / exp > 9)
-exp = exp * 10;
+exp *= 10;
 while (exp != 0)
 {
-count = count + _putchar(num / exp + '0');
+count += _putchar(num / exp + '0');
 num = num % exp;
 exp = exp / 10;
 }
@@ -41,16 +42,16 @@ unsigned int num;
 x = va_arg(args, int);
 if (x < 0)
 {
-count = count + _putchar('-');
-x = -x;
+count += _putchar('-');
+num = x * -1;
 }
 else
 num = x;
 while (num / exp > 9)
-exp = exp * 10;
+exp *= 10;
 while (exp != 0)
 {
-count = count + _putchar(num / exp + '0');
+count += _putchar(num / exp + '0');
 num = num % exp;
 exp = exp / 10;
 }
