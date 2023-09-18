08c58a8a6e06ff1b356704487b9158240a639150
“Dino” <daringamaleldin@hotmail.com>
2023-09-18 07:44:39 -0700

    task0

---
+++ task0.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_char - to print char
+ * @args: list of arguments
+ *
+ * Return: to 1
+ */
+
+int print_char(va_list args)
+{
+	_putchar(va_arg(args, int);
+			return (1);
+}
+
+/**
+ * print_string - to print string
+ * @args: list of arguments
+ *
+ * Return: count
+ */
+
+int print_string(va_list args)
+{
+
+int count = 0;
+char *str = va_arg(args, char*);
+
+if (str == NULL)
+str = "(null)";
+
+while (str[count] != '\0')
+{
+	_putchar(str[count]);
+count++;
+}
+return (count);
+}
+
+/**
+ * print_percent - to print percent
+ * Return: 1
+ */
+
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
