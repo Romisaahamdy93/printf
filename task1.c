7c29dc67d86b293ac8cfd6bcaba4677ed0b3743e
Romisaa <romisaa.hamdy@yahoo.com>
2023-09-18 14:17:36 -0700

    task1

--- task1.c
+++ task1.c
@@ -30,4 +30,34 @@ exp = exp / 10;
 }
 return (count);
 }
-
+/**
+ * print_dec - prints decimal numbers
+ * @args: list of arguments
+ * Return: numbers of decimal numbers printed
+ */
+int print_dec(va_list args)
+{
+int x;
+int exp = 1;
+int count = 0;
+unsigned int num;
+x = va_arg(args, int);
+if (x < 0)
+{
+_putchar('-');
+count++;
+x = x * -1;
+}
+else
+num = x;
+while (num / exp > 9)
+exp = exp * 10;
+while (exp != 0)
+{
+_putchar(num / exp + '0');
+count++;
+num = num % exp;
+exp = exp / 10;
+}
+return (count);
+}
